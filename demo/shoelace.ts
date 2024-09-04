74e0fd06d9e44e6452cd764692cd3cb0d0b4051f
Roberto Vidal <roberto.vidal@ikumene.com>
2024-09-04 03:00:54 +0200

    lazy loading

---
+++ demo/shoelace.ts
@@ -0,0 +1,4 @@
+import "@shoelace-style/shoelace/dist/themes/light.css";
+import "@shoelace-style/shoelace/dist/components/tab/tab";
+import "@shoelace-style/shoelace/dist/components/tab-group/tab-group";
+import "@shoelace-style/shoelace/dist/components/tab-panel/tab-panel";
